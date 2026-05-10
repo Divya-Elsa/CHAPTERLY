7a08df1e0481d6aa7bbdbea5b91dfcf4a8ab618e
Divya Thomas <divyaelsathomas@gmail.com>
2026-05-10 09:26:27 +0530

    Spring Boot

---
+++ readingtracker/src/main/java/com/example/readingtracker/ReadingtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.readingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingtrackerApplication.class, args);
+	}
+
+}
---
+++ readingtracker/src/test/java/com/example/readingtracker/ReadingtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
